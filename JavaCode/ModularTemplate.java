ef411a4ff3734a080ae8a84b10c3e8429e56d1e3
Isaac-Schmidt <47532091+Isaac-Schmidt@users.noreply.github.com>
2020-06-18 18:17:35 -0500

    Template for mini-game

--- JavaCode/ModularTemplate.java
+++ JavaCode/ModularTemplate.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Scanner;
-
+ 
 public class ModularTemplate {
     private Scanner sc;
     public final String gameName = "Minigame";//identifies game. Replace with appropriate value.
